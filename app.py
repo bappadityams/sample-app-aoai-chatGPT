7cc8f620885cdbc65373df1ca688c87c17744672
Bappaditya Banerjee <104856952+bappadityams@users.noreply.github.com>
2024-01-05 20:35:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,11 @@ import openai
 import copy
 from azure.identity import DefaultAzureCredential
 from base64 import b64encode
-from flask import Flask, Response, request, jsonify, send_from_directory
+from flask import Flask, Response, request, jsonify,render_template, send_from_directory
 from dotenv import load_dotenv
+from flask_session import Session
+
+import app_config
 
 from backend.auth.auth_utils import get_authenticated_user_details
 from backend.history.cosmosdbservice import CosmosConversationClient
@@ -15,10 +18,50 @@ from backend.history.cosmosdbservice import CosmosConversationClient
 load_dotenv()
 
 app = Flask(__name__, static_folder="static")
+app.config.from_object(app_config)
+assert app.config["REDIRECT_PATH"] != "/", "REDIRECT_PATH must not be /"
+Session(app)
+
+app.jinja_env.globals.update(Auth=identity.web.Auth)  # Useful in template for B2C
+auth = identity.web.Auth(
+    session=session,
+    authority=app.config["AUTHORITY"],
+    client_id=app.config["CLIENT_ID"],
+    client_credential=app.config["CLIENT_SECRET"],
+)
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html", version=__version__, **auth.log_in(
+        scopes=app_config.SCOPE, # Have user consent to scopes during log-in
+        redirect_uri=url_for("auth_response", _external=True), # Optional. If present, this absolute URL must match your app's redirect_uri registered in Azure Portal
+        prompt="select_account",  # Optional. More values defined in  https://openid.net/specs/openid-connect-core-1_0.html#AuthRequest
+        ))
+
+
+@app.route(app_config.REDIRECT_PATH)
+def auth_response():
+    result = auth.complete_log_in(request.args)
+    if "error" in result:
+        return render_template("auth_error.html", result=result)
+    return redirect(url_for("index"))
+
+
+@app.route("/logout")
+def logout():
+    return redirect(auth.log_out(url_for("index", _external=True)))
+
 
 # Static Files
 @app.route("/")
 def index():
+     if not (app.config["CLIENT_ID"] and app.config["CLIENT_SECRET"]):
+        # This check is not strictly necessary.
+        # You can remove this check from your production code.
+        return render_template('config_error.html')
+    if not auth.get_user():
+        return redirect(url_for("login"))
     return app.send_static_file("index.html")
 
 @app.route("/favicon.ico")
